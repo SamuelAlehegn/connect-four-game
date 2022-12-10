6706c392f8a98583f7d70e385548349ba78b5138
SamuelAlehegn <hadis.samuelalehegn@gmail.com>
2022-12-10 20:19:39 +0300

    The basic js script is finished.

---
+++ script.js
@@ -0,0 +1,121 @@
+var playerOne = prompt("Player One: Enter Your Name, you will be Blue");
+var playerOneColor = 'rgb(86, 151, 255)';
+
+var playerTwo = prompt("Player Two: Eneter Your Name, you will be Red");
+var playerTwoColor = 'rgb(237, 45, 73)';
+
+var game_on = true;
+var table = $('table tr');
+
+function reportWin(rowNum, colNum){
+   console.log("You won starting at this row, col")
+   console.log(rowNum) 
+   console.log(colNum) 
+}
+
+function changeColor(rowIndex, colIndex, color){
+   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-colr', color);
+}
+
+function returnColor(rowIndex, colIndex){
+   return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-colr');
+}
+
+function checkBottom(colIndex){
+    var colorReport = returnColor(5, colIndex);
+    for (var row = 5; row > -1; row--){
+        colorReport = returnColor(row, colIndex);
+        if (colorReport === 'rgb(128, 128, 128)'){
+            return row
+        }
+    }
+}
+
+function colorMatchCheck(one, two, three, four){
+    return (one == two && one == three && one == four && one !== 'rgb(128, 128, 128)' && one !== undefined);
+}
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1) ,returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Vertical Wins
+function verticalWinCheck() {
+  for (var col = 0; col < 7; col++) {
+    for (var row = 0; row < 3; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col) ,returnColor(row+2,col), returnColor(row+3,col))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+// Check for Diagonal Wins
+function diagonalWinCheck() {
+  for (var col = 0; col < 5; col++) {
+    for (var row = 0; row < 7; row++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row+1,col+1) ,returnColor(row+2,col+2), returnColor(row+3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else if (colorMatchCheck(returnColor(row,col), returnColor(row-1,col+1) ,returnColor(row-2,col+2), returnColor(row-3,col+3))) {
+        console.log('diag');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+
+var currentPlayer = 1;
+var currentName = playerOne;
+var currentColor = playerOneColor;
+
+$('h3').text(playerOne + " it is your turn, pick a column to drop in!")
+
+$('.board button').on('click', function(){
+
+    var col = $(this).closet('td').index();
+    var bottomAvail = checkBottom(col);
+    changeColor(bottomAvail, col, currentColor);
+
+    if (horizontalWinCheck() || verticalWinCheck() || diagonalWinCheck()){
+        $('h1').text(currentName + " You Have Won!");
+        $('h3').fadeOut('fast');
+        $('h2').adeOut('fast');
+    }
+
+currentPlayer = currentPlayer * -1;
+
+if (currentPlayer == 1){
+    currentName = playerOne;
+    $('h3').text(currentName + " it is your turn.")
+    currentColor = playerOneColor;
+
+}else{
+    currentName = playerTwo;
+    $('h3').text(currentName + " it is your turn.")
+    currentColor = playerTwoColor;
+
+}
+}) 
+
+ 
